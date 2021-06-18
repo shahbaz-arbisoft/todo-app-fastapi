9447a054a944a48b59f90fd2f202ca400b06519f
shahbaz_shabbir <shahbaz.shabbir@fiveriverstech.com>
2021-06-19 02:56:04 +0500

    CRUD operations test

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
-from typing import Optional
 from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get('/')
-async def main():
-	return {'Hello': 'Hii'}
+
+@app.get('/todo/')
+def retrive_all_todo():
+    return {'Hello': 'Get'}
+
+
+@app.post('/todo/')
+def create_todo():
+    return {'Hello': 'Post'}
+
+
+@app.put("/todo/{id}")
+def update_todo(id):
+    return {'Hello': 'Put-' + str(id)}
+
+
+@app.get("/todo/{id}")
+def retrive_todo(id):
+    return {'Hello': 'Get-' + str(id)}
+
+
+@app.delete("/todo/{id}")
+def delete_todo(id):
+    return {'Hello': 'Delete-' + str(id)}
